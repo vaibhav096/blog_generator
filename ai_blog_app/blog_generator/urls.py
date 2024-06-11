4944c337df92ccf4139b9cc1b88d2ac02a47914a
vaibhav096 <vaibhavajadhav96@gmail.com>
2024-06-11 19:14:57 +0530

    updates the code files

--- ai_blog_app/blog_generator/urls.py
+++ ai_blog_app/blog_generator/urls.py
@@ -7,5 +7,6 @@ urlpatterns =[
     path('signup', views.user_signup, name='signup'),
     path('logout', views.user_logout, name='logout'),
     path('generate-blog', views.generate_blog, name='generate-blog'),
-
+    path('blog-list', views.blog_list, name='blog-list'),
+    path('blog-details/<int:pk>', views.blog_details, name='blog-details'),
 ]
--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -2,16 +2,130 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+import os 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+import assemblyai as aai
+from django.http import JsonResponse
+import json
+import openai
+from openai import OpenAI
+from .models import BlogPost
+# for use of gemini model 
+import google.generativeai as genai
+from IPython.display import Markdown
+
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+
+ASSEMBLYAI_API_KEY=os.getenv("ASSEMBLYAI_API_KEY")
+
+from pytube import YouTube
+import re
 
 @login_required
 def index(request):
     return render(request,'index.html')
 
 
+@csrf_exempt
 def generate_blog(request):
-    pass
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            yt_link = data['link']
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invalid data sent'}, status=400)
 
 
+        # get yt title
+        title = yt_title(yt_link)
+
+        # get transcript
+        transcription = get_transcription(yt_link)
+        if not transcription:
+            return JsonResponse({'error': " Failed to get transcript"}, status=500)
+
+
+        # use OpenAI to generate the blog
+        blog_content = generate_blog_from_transcription(transcription)
+        if not blog_content:
+            return JsonResponse({'error': " Failed to generate blog article"}, status=500)
+
+        # save blog article to database
+        new_blog_article = BlogPost.objects.create(
+            user=request.user,
+            youtube_title=title,
+            youtube_link=yt_link,
+            generated_content=blog_content,
+        )
+        new_blog_article.save()
+
+        # return blog article as a response
+        return JsonResponse({'content': blog_content})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+def yt_title(link):
+    yt = YouTube(link)
+    title = yt.title
+    return title
+
+def download_audio(link):
+    yt = YouTube(link)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path=settings.MEDIA_ROOT)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    if not os.path.exists(new_file):
+        os.rename(out_file, new_file)
+    else:
+        os.remove(out_file)
+    return new_file
+
+def get_transcription(link):
+    audio_file = download_audio(link)
+    aai.settings.api_key = ASSEMBLYAI_API_KEY
+    print(f"API Key Set: {aai.settings.api_key}") 
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audio_file)
+
+    return transcript.text
+
+def generate_blog_from_transcription(transcription):
+
+    prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article containing words less than 1000 , write descriptive blog article dont use styling, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
+    
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    pattern =  r"\*\*\*|##|,+"
+
+
+    response = model.generate_content(prompt)
+    content = response.text.strip()  # Access the generated text
+    content = re.sub(pattern, "", content, flags=re.MULTILINE)
+
+    return content
+
+
+
+def blog_list(request):
+    blog_articles = BlogPost.objects.filter(user=request.user)
+    return render(request, "all-blogs.html", {'blog_articles': blog_articles})
+
+def blog_details(request, pk):
+    blog_article_detail = BlogPost.objects.get(id=pk)
+    if request.user == blog_article_detail.user:
+        return render(request, 'blog-details.html', {'blog_article_detail': blog_article_detail})
+    else:
+        return redirect('/')
 
 def user_login(request):
     if request.method =='POST':
